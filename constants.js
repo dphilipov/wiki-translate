f4e5b487eb6be4a9a876eb0ff1ff9c774af66282
Dimitar Filipov <grinndev@protonmail.com>
2023-01-31 21:30:16 +0200

    add constants

---
+++ constants.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const constants = {
+  ALLOW_FILE_OVERWRITE: false,
+  ARTICLE_TITLES: ['!?!'],
+  AUTH_KEY: process.env.AUTH_KEY,
+  GLOSSARY_ID: '150bf192-7f95-4e2d-80d9-327c8f6e55f6',
+  INPUT_FOLDER: 'input',
+  OUTPUT_FOLDER: 'output',
+  WIKI_URL: 'https://www.shadowiki.de',
+};

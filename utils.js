8dd728d072ad3126a3258128fa1285ad4fa4ee10
Dimitar Filipov <grinndev@protonmail.com>
2025-09-30 18:44:27 +0300

    update the filtering logic

--- utils.js
+++ utils.js
@@ -5,7 +5,7 @@ import { constants } from './constants.js';
 export async function fileExists(pageTitle) {
   const files = await fs.readdir(constants.OUTPUT_FOLDER);
 
-  const filteredFiles = files.filter(file => file.includes(pageTitle));
+  const filteredFiles = files.filter(file => file === pageTitle);
 
   const fileExists = filteredFiles.length > 0;
 
